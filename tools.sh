d822429de67a65454913a3d2020b4d164bb0d9a7
Satya <strikersatya2@gmail.com>
2024-04-20 17:49:44 +0530

    Added functions and updated tools.sh script

--- tools.sh
+++ tools.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 TOOLS='unzip terraform packer docker kops jq'
+
+function apt_tools {
+    apt update
+    apt install -y unzip jq
+}
+
+tform() {
+    wget https://releases.hashicorp.com/terraform/1.8.1/terraform_1.8.1_linux_amd64.zip
+    unzip terraform_1.8.1_linux_amd64.zip
+    mv terraform /usr/local/bin
+    terraform version
+}
+
+packerr() {
+    wget https://releases.hashicorp.com/packer/1.10.2/packer_1.10.2_linux_amd64.zip
+    unzip packer_1.10.2_linux_amd64.zip
+    mv packer /usr/local/bin
+    packer version
+}
+
+kopss() {
+    wget https://github.com/kubernetes/kops/releases/download/v1.28.4/kops-linux-amd64
+    mv kops-linux-amd64 /usr/local/bin/kops
+    kops version
+}
+
+dockerr() {
+    curl https://get.docker.com | bash
+}
+
 for tool in $TOOLS; do
     $tool version 2>/dev/null
     if [ $? -eq 0 ]; then
         echo "$tool is already installed"
     else
         echo "Let's install $tool"
+        if [ $tool = 'unzip' -o $tool = 'jq' ]; then
+            apt_tools
+        fi
+
     fi
 done
