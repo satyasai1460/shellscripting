4ed724b7034e8bcd3ea5898513b80c878dcffd11
Satya <strikersatya2@gmail.com>
2024-04-20 13:35:22 +0530

    Updated License script

--- license.sh
+++ license.sh
@@ -2,7 +2,7 @@
 Current_Year=2024
 DOB=$1
 
-if [ $(expr $Current_Year - $DOB) -gt 18]; then
+if [ $(expr $Current_Year - $DOB) -gt 18 ]; then
     echo "You are elecgible foe Driving License"
 else
     echo "You are not Eligible for Driving License"
