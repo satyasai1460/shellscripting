efd93e7a731df55c3971828140811919eff00b47
Satya <strikersatya2@gmail.com>
2024-04-20 13:40:10 +0530

    Added License script

--- license.sh
+++ license.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 Current_Year=2024
 DOB=$1
-CurrentAge= $(expr $Current_Year - $DOB)
+CurrentAge=$(expr $Current_Year - $DOB)
 echo "Your Current Age is $CurrentAge"
 if [ $CurrentAge -gt 18 ]; then
     echo "You are elecgible foe Driving License"
