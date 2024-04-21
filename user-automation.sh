e68ef916a8c5cef118fea3f3f40e0dbc7b9dc3dd
Satya <strikersatya2@gmail.com>
2024-04-21 16:35:47 +0530

    Updated User-automation script

--- user-automation.sh
+++ user-automation.sh
@@ -15,7 +15,16 @@ if [ $# -gt 0 ]; then
         echo "$USER1 already exists, Please try with different username."
     else
         echo "Username $USER1 is available...Lets create the user with username $USER1"
-        # useradd $USER1 -m --shell /bin/bash
+        sudo useradd $USER1 -m --shell /bin/bash
+        #Generating a Random Password for the user
+        SPEC=$(echo '!@#$%^&*()_' | fold -1 | shuf | head -1)
+        PASSWORD="India@${RANDOM}${spec}"
+        echo "$USER1:$PASSWORD" | sudo chpasswd
+        #Forcing the user to change the password on Next Login
+        passwd -e $USER1
+        #Adding the user to sudoers file
+        sudo usermod -aG sudo $USER1
+        echo "Successfully created the user with the username $USER1 and password $PASSWORD"
     fi
 else
     echo "Please give atleast 1 Argument"
