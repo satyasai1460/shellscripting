b31d41dc682d6d627fb48709f34911fa469b8235
Satya <strikersatya2@gmail.com>
2024-04-23 23:32:16 +0530

    Updated du.sh  script

--- du.sh
+++ du.sh
@@ -1,8 +1,8 @@
 FILESYSTEM=$(df -h | egrep -v 'loop|xvda|tmpfs' | awk -F ' ' '{print $1}' | tail -1 | sed "s/%//g")
 PERCENTAGE=$(df -h | egrep -v 'loop|xvda|tmpfs' | awk -F ' ' '{print $5}' | tail -1 | sed "s/%//g")
 
-echo "This is the file system used the disk space $FILESYSTEM"
-echo "$PERCENTAGE% disk space is used"
+echo "This is the file system used the disk space :  $FILESYSTEM"
+echo "$PERCENTAGE% of disk space is used"
 
 if [ $PERCENTAGE -lt 50 ]; then
     echo "You've used $PERCENTAGE% of Disk space...It's in good state"
