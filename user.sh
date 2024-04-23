6a9a43d0e417824471e220db9d6efc157ef38b9a
Satya <strikersatya2@gmail.com>
2024-04-23 20:36:35 +0530

    Updated user.sh  script

--- user.sh
+++ user.sh
@@ -7,12 +7,24 @@
 
 USER=$1
 EXISTING_USER=$(cat /etc/passwd | awk -F ':' '{print $1}' | grep -i $USER)
+PASSWORD_GEN=$(echo $RANDOM '!@#$%^&*()_' | fold -1 | shuf | head -1)
+PASSWORD="India@${RANDOM}${PASSWORD_GEN}"
 if [ $# -gt 0 ]; then
     echo "Checking if $USER is already Exists"
     if [ "${USER}" = "${EXISTING_USER}" ]; then
         echo "Sorry, Username $USER is already taken. Please pick a New Username"
     else
         echo "$USER Username is available, Let's Create the user with ${USER}"
+        #Adding the user with home directory and shell
+        sudo useradd -m $USER --shell /bin/bash
+        #Generating the password for the user
+        PASSWORD="India@${RANDOM}${PASSWORD_GEN}"
+        #Forcing the user to change the password
+        echo "$USER:$PASSWORD" | sudo chpasswd
+        #Forcing the user to reset the password at first login
+        passwd -e $USER
+        sudo usermod -aG sudo $USER
+        echo "Successfully added the user with password"
     fi
 else
     echo "Please give atleast one argument"
