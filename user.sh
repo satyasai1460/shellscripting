25562c55a9da2a9af1e4e9734d48a65a3abe31a7
Satya <strikersatya2@gmail.com>
2024-04-23 21:58:29 +0530

    Updated user.sh  script

--- user.sh
+++ user.sh
@@ -25,7 +25,10 @@ if [ $# -gt 0 ]; then
         echo "$USER:$PASSWORD" | sudo chpasswd
         #Forcing the user to reset the password at first login
         passwd -e $USER
-        sudo usermod -aG sudo $USER
+        read -p "Does this user needs to be SUDO (yes/no) :" RESPONSE
+        if [ "${RESPONSE}" = 'yes' -o "${RESPONSE}" = 'YES' -o "${RESPONSE}" = 'y' -o "${RESPONSE}" = 'Y' ]; then
+            sudo usermod -aG sudo $USER
+        fi
         echo -e "${GREEN}Successfully added the $USER with password ${PASSWORD}${RESET}"
     fi
 else
