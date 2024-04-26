a220e01c024b7bffd7110dae0237dc902752770d
Satya <strikersatya2@gmail.com>
2024-04-26 23:06:21 +0530

    Updated palindrome2 test script

--- findDiskSpace.sh
+++ findDiskSpace.sh
@@ -2,15 +2,15 @@ WORD=$1
 LENGTH=$(expr ${#WORD} - 1)
 NEWWORD=''
 
-if [ $# -le 0 ]; then
-    echo "Please give atleast 1 argument"
-else
+# if [ $# -le 0 ]; then
+#     echo "Please give atleast 1 argument"
+# else
 
-    for ((i = $LENGTH; i >= 0; i--)); do
-        CHAR=${WORD:$i:1}
-        NEWWORD=$NEWWORD$CHAR
-    done
-fi
+for ((i = $LENGTH; i >= 0; i--)); do
+    CHAR=${WORD:$i:1}
+    NEWWORD=$NEWWORD$CHAR
+done
+# fi
 
 if [ ${WORD} = ${NEWWORD} ]; then
     echo "$WORD is PALINDROME"
