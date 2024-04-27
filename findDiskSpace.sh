1b40814a2dbb1adb86957e60caba582a3137f032
Satya <strikersatya2@gmail.com>
2024-04-27 20:10:07 +0530

    Added findDiskSpace  script

--- findDiskSpace.sh
+++ findDiskSpace.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-read -p "Enter a word to check it was a Palindrome or Not : " INPUT
-WORD=$(echo ${INPUT} | tr '[:upper:]' '[:lower:]')
-LENGTH=$(expr ${#WORD} - 1)
-NEWWORD=''
 
-if [ $# -le 0 ]; then
-    echo "Please give atleast 1 argument"
-else
+# 1.Total Space
+# 2.File system
+# 3.Used space
+# 4.Used space percentage
+# 5.Remaining space Available
 
-    for ((i = $LENGTH; i >= 0; i--)); do
-        CHAR=${WORD:$i:1}
-        NEWWORD=$NEWWORD$CHAR
-    done
-fi
+TOTALSPACE=$(df -h | grep -i root | awk -F ' ' '{print $2}' | sed "s/G//g")
+FILESYSTEM=$(df -h | grep -i root | awk -F ' ' '{print $1}')
+USEDSPACE=$(df -h | grep -i root | awk -F ' ' '{print $3}' | sed "s/G//g")
+USEDSPACEPERCENTAGE=$(df -h | grep -i root | awk -F ' ' '{print $5}' | sed "s/%//g")
+REMAININGSPACEPERCENTAGE=$(expr 100 - ${USEDSPACEPERCENTAGE})
+AVAILABLESPACE=$(df -h | grep -i root | awk -F ' ' '{print $4}' | sed "s/G//g")
 
-if [ ${WORD} = ${NEWWORD} ]; then
-    echo "$WORD is PALINDROME"
-else
-    echo "$WORD is not PALINDROME"
-fi
+echo "The Total space for the ${FILESYSTEM} filesystem was ${TOTALSPACE}G"
+echo "The Space used by the filesystem  ${FILESYSTEM} was ${USEDSPACE}G i.e, (${USEDSPACEPERCENTAGE}%/100)"
+echo "The Remaining Available Space was ${AVAILABLESPACE}G"
